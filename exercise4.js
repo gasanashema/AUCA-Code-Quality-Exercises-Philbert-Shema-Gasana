81925c79d0db56a7b0301315c430fbcc3e3f54c8
Shema <shemaphilbert8@gmail.com>
2025-09-10 15:10:47 +0200

    Refactoed exercise 4

---
+++ exercise4.js
@@ -0,0 +1,21 @@
+/*
+function findEvenNumbers(numbers) {
+  var evenNumbers = [];
+  for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      evenNumbers.push(numbers[i]);
+    }
+  }
+  return evenNumbers;
+}
+*/
+
+function getEvenNumbers(numbers) {
+  let evenNumbers = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      evenNumbers.push(numbers[i]);
+    }
+  }
+  return evenNumbers;
+}
