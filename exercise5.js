14b43a3b0b7a30cd9db68557ea572c8d438ce135
Shema <shemaphilbert8@gmail.com>
2025-09-10 15:34:08 +0200

    refactored exercise 5

--- exercise5.js
+++ exercise5.js
@@ -11,9 +11,9 @@ function calculateFactorial(n) {
   }
 }
 */
-function calculateFactorial(n) {
+function Factorial(n) {
     if (n === 0 || n === 1) return 1;
-    
+
     let factorial = 1;
     for (let i = 2; i <= n; i++) {
         factorial *= i;
